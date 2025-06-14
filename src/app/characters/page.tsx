b9d7a4adced66020e1f14f649254994fe6834dbc
LemosFTW <rl.fernandes@campus.fct.unl.pt>
2025-06-14 17:00:34 -0300

    Changing the implementation to useEffect and useState, it caused an error fetching with the other implementation.

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,15 +1,52 @@
 "use client";
-import { use } from "react";
+import { useState } from "react";
 import { get } from "@/services/axios";
 import CharacterCardComponent from "@/components/characterCardComponent";
+import PaginationComponent from "@/components/paginationComponent";
+import { useEffect } from "react";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import ErrorDisplay from "@/components/ErrorDisplay";
 
-const charactersPromise = get("/people").then((response) => {
-  if (response.status === 200) return response.data;
-  throw new Error("Failed to fetch characters");
-});
+const fetchCharacters = async (page: number) =>
+  await get(`/people?page=${page}&limit=10`).then((response) => {
+    if (response.status === 200) return response.data;
+    throw new Error("Failed to fetch characters");
+  });
 
 export default function CharactersPage() {
-  const characters = use(charactersPromise);
+  const [page, setPage] = useState(1);
+  const [characters, setCharacters] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const data = await fetchCharacters(page);
+        setCharacters(data);
+      } catch (err) {
+        setError(err as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getCharacters();
+  }, [page]);
+
+  const handlePreviousPage = () => {
+    if (page > 1) setPage(page - 1);
+  };
+
+  const handleNextPage = () => {
+    if (characters && characters.next) setPage(page + 1);
+  };
+
+  if (loading) return <LoadingSpinner />;
+  if (error) return <ErrorDisplay message={error.message} />;
+  if (!characters) return <ErrorDisplay message={"No characters found"} />;
 
   return (
     <div className="container mx-auto px-4 py-8 min-h-screen">
@@ -28,6 +65,13 @@ export default function CharactersPage() {
           />
         ))}
       </div>
+      <PaginationComponent
+        currentPage={page}
+        hasNext={!!characters.next}
+        hasPrevious={!!characters.previous}
+        onNext={handleNextPage}
+        onPrevious={handlePreviousPage}
+      />
     </div>
   );
 }
