3b6d6dce7a7bbe6e75a5084962c82a1ff635445f
LemosFTW <rl.fernandes@campus.fct.unl.pt>
2025-06-15 22:08:17 -0300

    feat UI: Apply stars background to all pages. Center the stars tp enhance zoom animation with translation.

---
+++ src/components/BackgroundStars.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BackgroundStars: React.FC = () => {
+  return (
+    <div className="fixed inset-0 -z-10">
+      <div className="main"></div>
+    </div>
+  );
+};
+
+export default BackgroundStars;
